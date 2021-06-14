b83b44647b75f71f49ca8dcd0ecc9357b1be8464
lui0403 <lumanpower2004@gmail.com>
2021-06-14 17:25:59 +0900

    wrote js file

--- src/js/message.js
+++ src/js/message.js
@@ -5,8 +5,8 @@ function myMessage() {
 
         let div1 = document.createElement("div");
         let div2 = document.createElement("div");
-        div2.classList.add("mine", "messages");
         let div3 = document.createElement("div");
+        div2.classList.add("mine", "messages");
         div3.classList.add("message", "last");
         div3.textContent = message;
         div2.appendChild(div3);
@@ -17,7 +17,7 @@ function myMessage() {
         div1.appendChild(div2);
 
         let div = document.getElementById("chat");
-        div.append(div1);
+        div.appendChild(div1);
 
         //textをクリアする
         let inputMessage = document.getElementById("message");
@@ -27,4 +27,38 @@ function myMessage() {
     } else {
         alert("メッセージが未入力です。")
     }
+}
+
+function botMessage(message) {
+  let date = new Date();
+  let div1 = document.createElement("div");
+  let div2 = document.createElement("div");
+  div2.classList.add("other", "messages");
+
+  let divAvatar = document.createElement("div");
+  divAvatar.classList.add("avatar");
+  let i = document.createElement("i");
+  i.classList.add("fas", "fa-user-circle", "fa-2x");
+  divAvatar.appendChild(i);
+  div2.appendChild(divAvatar);
+
+  let div3 = document.createElement("div");
+  let spanUser = document.createElement("span");
+  spanUser.classList.add("user-name");
+  spanUser.textContent = "経営者Aさん";
+  div3.appendChild(spanUser);
+  let divMessage = document.createElement("div");
+  divMessage.classList.add("message", "last");
+  divMessage.textContent = message;
+  div3.appendChild(divMessage);
+  let divTime = document.createElement("div");
+  divTime.classList.add("time");
+  divTime.textContent = date.toLocaleString();
+  div3.appendChild(divTime);
+  div2.appendChild(div3);
+
+  div1.appendChild(div2);
+
+  let div = document.getElementById("chat");
+  div.append(div1);
 }
